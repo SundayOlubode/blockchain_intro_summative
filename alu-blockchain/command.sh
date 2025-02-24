921bf3283659fec6ad7a38fbacd65756fcca5653
Sam <s.olubode@alustudent.com>
2025-02-24 22:33:15 +0100

    commands

--- alu-blockchain/command.sh
+++ alu-blockchain/command.sh
@@ -1,2 +1,3 @@
+rm -r ./backups ./wallets.dat ./transactions.dat ./txpool.dat ./kitchens.txt ./profiles.dat
 gcc -Wall -Werror -Wextra -pedantic -std=c99 main.c alu_blockchain.c config.c wallet.c profile.c -o alu_payment -lssl -lcrypto
-# ./alu_payment
+./alu_payment
